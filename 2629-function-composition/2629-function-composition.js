8acc520b893fc9807bb75c7c9c701494e581fac0
Dhruvil Patel <155451633+dhruvil-10@users.noreply.github.com>
2026-07-09 16:54:09 +0530

    Time: 43 ms (96.56%), Space: 55.4 MB (20.17%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        if(!functions.length) return x;
+
+        for(let i = functions.length-1 ; i>=0 ; i--)
+        {
+            const cur= functions[i];
+            x= cur(x);
+        }
+        return x;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
