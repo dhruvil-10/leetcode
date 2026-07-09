db5aacac044debc099793290acfac8c26b03702b
Dhruvil Patel <155451633+dhruvil-10@users.noreply.github.com>
2026-07-09 17:15:53 +0530

    Time: 60 ms (5.5%), Space: 54 MB (25.95%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let ar = [] ;
+    for(let i = 0 ; i<arr.length ; i++)
+    {
+        if(fn(arr[i],i)) ar.push(arr[i]);
+    }
+    return ar;
+    
+};
